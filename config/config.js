98f96847d4eb2b1fbc634e8a32f275c0442bd58d
ATT273 <anhtuan273@gmail.com>
2021-10-28 21:54:51 +0700

    move db_connect to config file

---
+++ config/config.js
@@ -0,0 +1,3 @@
+const DB_CONNECTION = 'mongodb+srv://tuanta:26644888266mongo@mongodb.f0x9t.mongodb.net/MongoDB?retryWrites=true&w=majority';
+
+module.exports = { DB_CONNECTION }
--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const { DB_CONNECTION } = require('./config/config');
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
@@ -9,7 +10,6 @@ const port = 3000;
 
 app.use(cors({ origin: '*' }))
 app.use(bodyParser.json());
-
 // import routes
 const postRoute = require('./routes/posts');
 const transactionRoute = require('./routes/transactions');
@@ -21,7 +21,7 @@ app.get('/', (req, res) => {
     res.send('Hello World!');
 })
 // connect to db
-mongoose.connect(process.env.DB_CONNECTION, { useNewUrlParser: true }, () => console.log('connected to db!'))
+mongoose.connect(DB_CONNECTION, { useNewUrlParser: true }, () => console.log('connected to db!'))
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
 })
